1b603f2c784b2621f71985b2c94145b9831f1a58
OluhleM <222419636@mycput.ac.za>
2025-05-11 16:19:52 +0200

    Made a few adjustments.

--- src/main/java/za/ac/cput/domain/FoodType.java
+++ src/main/java/za/ac/cput/domain/FoodType.java
@@ -3,4 +3,51 @@ package za.ac.cput.domain;
 public class FoodType {
     private  String foodTypeID;
     private  String foodTypeName;
+
+    private FoodType() {
+
+    }
+
+    private FoodType(FoodType.Builder builder){
+
+    }
+
+    public String getFoodTypeID() {
+        return foodTypeID;
+    }
+
+    public String getFoodTypeName() {
+        return foodTypeName;
+    }
+
+    @Override
+    public String toString() {
+        return "FoodType{" +
+                "foodTypeID=" + foodTypeID +
+                ", foodTypeName='" + foodTypeName + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String foodTypeID;
+        private String foodTypeName;
+
+        public Builder setFoodTypeID(String foodTypeID) {
+            this.foodTypeID = foodTypeID;
+            return this;
+        }
+
+        public Builder setFoodTypeName(String FoodTypeName) {
+            this.foodTypeName = foodTypeName;
+            return this;
+        }
+
+        public FoodType copy(FoodType foodType){
+            this.foodTypeID = foodType.foodTypeID;
+            this.foodTypeName = foodType.foodTypeName;
+            return this.build();
+        }
+
+        public FoodType build(){return new FoodType(this);}
+    }
 }
