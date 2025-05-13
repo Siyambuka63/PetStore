fc95ff12f8416584eb7874009ed0da264499192d
OluhleM <222419636@mycput.ac.za>
2025-05-13 21:13:09 +0200

    Ammended ProductFactory.

--- src/main/java/za/ac/cput/factory/product/ProductFactory.java
+++ src/main/java/za/ac/cput/factory/product/ProductFactory.java
@@ -1,11 +1,13 @@
 package za.ac.cput.factory.product;
+/*Oluhle Makhaye
+ * 222419636*/
 
 import za.ac.cput.domain.product.Product;
 import za.ac.cput.util.Helper;
 
 public class ProductFactory {
     public static Product createProduct(int productID, String productName, String description) {
-        if (Helper.isNullOrEmpty(productName) || Helper.isNullOrEmpty(description) || Helper.isIntValid(productID)) return null;
+        if (Helper.isNullOrEmpty(productName) || Helper.isNullOrEmpty(description)) return null;
         // if (!Helper.isNullOrEmpty(productName) && !Helper.isNullOrEmpty(description)) return null;
         return new Product.Builder()
                 .setProductID(productID)
