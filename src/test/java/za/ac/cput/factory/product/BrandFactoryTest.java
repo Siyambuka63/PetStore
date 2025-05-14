fb5a72ed0683feadf50385a042e1ebd3aaa14a3c
OluhleM <222419636@mycput.ac.za>
2025-05-14 23:22:36 +0200

    ammended BrandFactoryTest.

--- src/test/java/za/ac/cput/factory/product/BrandFactoryTest.java
+++ src/test/java/za/ac/cput/factory/product/BrandFactoryTest.java
@@ -4,20 +4,24 @@ import org.junit.jupiter.api.Test;
 import za.ac.cput.domain.product.Brand;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class BrandFactoryTest {
+class BrandFactoryTest {
+
+    private static Brand brandWithoutName = BrandFactory.createBrand(1,"");
+    private static Brand validBrand = BrandFactory.createBrand(2,"Montego");
+
+
     @Test
-    void createBrand(){
-        Brand brand = BrandFactory.createBrand(1,"Montego");
-        assertNotNull(brand);
-        System.out.println(brand.toString());
+    void createValidBrand(){
+        assertNotNull(validBrand);
+        System.out.println(validBrand);
     }
 
     @Test
     void createBrandWithoutBrandName(){
-        Brand brand = BrandFactory.createBrand(1,null);
-        assertNotNull(brand);
-        System.out.println(brand.toString());
+        assertNull(brandWithoutName);
+        System.out.println(brandWithoutName);
     }
 
 //    @Test
