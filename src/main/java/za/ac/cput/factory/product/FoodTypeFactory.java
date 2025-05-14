5c6a44dc98a404ab9df60b046eb68b60ae0c3b4a
OluhleM <222419636@mycput.ac.za>
2025-05-14 23:23:58 +0200

    ammended FoodTypeFactory.

--- src/main/java/za/ac/cput/factory/product/FoodTypeFactory.java
+++ src/main/java/za/ac/cput/factory/product/FoodTypeFactory.java
@@ -3,14 +3,16 @@ package za.ac.cput.factory.product;
  * 222419636*/
 
 import za.ac.cput.domain.product.FoodType;
+import za.ac.cput.domain.product.PetType;
 import za.ac.cput.util.Helper;
 
 public class FoodTypeFactory {
-    private static FoodType createFoodType(int foodTypeID, String foodTypeName) {
+    public static FoodType createFoodType(int foodTypeID, String foodTypeName, PetType petType) {
         if (Helper.isNullOrEmpty(foodTypeName)) return null;
         return new FoodType.Builder()
                 .setFoodTypeID(foodTypeID)
                 .setFoodTypeName(foodTypeName)
+                .setPetType(petType)
                 .build();
     }
 }
