cb317cda38bca939d1f1c748048e575f4b427e64
Elethu427 <221195521@mycput.ac.za>
2025-05-18 03:07:49 +0200

    factorytests

---
+++ src/test/java/za/ac/cput/factory/cart/CartFactoryTest.java
@@ -0,0 +1,12 @@
+package za.ac.cput.factory.cart;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartFactoryTest {
+
+    @Test
+    void createCart() {
+    }
+}
---
+++ src/test/java/za/ac/cput/factory/product/LifeStageFactoryTest.java
@@ -0,0 +1,12 @@
+package za.ac.cput.factory.product;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LifeStageFactoryTest {
+
+    @Test
+    void createLifeStage() {
+    }
+}
