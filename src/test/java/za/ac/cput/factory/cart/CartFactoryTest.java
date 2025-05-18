6b45ca6649e49aa139fcd8fe28fe83e65cee8d49
Elethu427 <221195521@mycput.ac.za>
2025-05-18 20:29:24 +0200

    factorytests

--- src/test/java/za/ac/cput/factory/cart/CartFactoryTest.java
+++ src/test/java/za/ac/cput/factory/cart/CartFactoryTest.java
@@ -1,12 +1,33 @@
 package za.ac.cput.factory.cart;
 
 import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.cart.Cart;
+import za.ac.cput.domain.cart.CartItem;
+import za.ac.cput.domain.order.OrderItem;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CartFactoryTest {
 
+ //   List<CartItem> items = new ArrayList<>();
+ //   CartItem items = new CartItem(100 , 2557, 2);
+ //   CartItem.add(items);
+    private static Cart cartWithoutItems = CartFactory.createCart(101,2556,null);
+ //   private static Cart cartWithItems = CartFactory.createCart(102,2556,);
+
     @Test
-    void createCart() {
+    public void createCartWithoutItems() {
+        assertNull(cartWithoutItems);
+        System.out.println(cartWithoutItems.toString());
+    }
+
+    @Test
+    public void createCartWithItems() {
+   //     assertNotNull(cartWithItems.);
+   //     System.out.println(cartWithItems.toString());
     }
 }
