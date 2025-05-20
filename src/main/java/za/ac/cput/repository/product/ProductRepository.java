b73a96b0e41f6ad191e2e1283ef5ce3c4660d839
OluhleM <222419636@mycput.ac.za>
2025-05-20 14:55:45 +0200

    Ammended ProductRepository class.

--- src/main/java/za/ac/cput/repository/product/ProductRepository.java
+++ src/main/java/za/ac/cput/repository/product/ProductRepository.java
@@ -2,5 +2,14 @@ package za.ac.cput.repository.product;
 /*Oluhle Makhaye
 * 222419636*/
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.product.Product;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Optional<Product> findByName(String name);
+    Optional<Product> findById(Integer id);
 }
