e27b444a408b3deb58f74beb415b584c319cf944
OluhleM <222419636@mycput.ac.za>
2025-07-21 16:00:13 +0200

    Ammended ProductService class.

--- src/main/java/za/ac/cput/service/ProductService.java
+++ src/main/java/za/ac/cput/service/ProductService.java
@@ -4,7 +4,7 @@ import za.ac.cput.domain.Product;
 
 import java.util.List;
 
-public interface IProductService extends IService<Product, Integer> {
+public interface ProductService extends IService<Product, Integer> {
     List<Product> getAll();
 
     Product findByProductName(String productName);
