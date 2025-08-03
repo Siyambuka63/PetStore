0cb4774e6fbcb39e65e1292e7f25a887c7a56ded
AthieX-34 <165181664+AthieX-34@users.noreply.github.com>
2025-08-03 21:42:27 +0200

    Update Address.java

--- src/main/java/za/ac/cput/domain/user/Address.java
+++ src/main/java/za/ac/cput/domain/user/Address.java
@@ -1,26 +1,45 @@
 package za.ac.cput.domain.user;
+//222319437
+//Athenkosi Mekana
+//Address Domain
 
 public class Address {
     private long addressID;
     private String streetAddress;
     private String complexDetail;
     private String suburb;
-    private String city;
     private Type type;
+    private String city;
 
-    private Address() {}
+    private String postalCode;
 
-    private Address(Builder builder) {
+    private Address() {
+
+    }
+ public Address (Builder builder) {
         this.addressID = builder.addressID;
         this.streetAddress = builder.streetAddress;
         this.complexDetail = builder.complexDetail;
         this.suburb = builder.suburb;
-        this.city = builder.city;
         this.type = builder.type;
+        this.city = builder.city;
 
+ }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressID=" + addressID +
+                ", streetAddress='" + streetAddress + '\'' +
+                ", complexDetail='" + complexDetail + '\'' +
+                ", suburb='" + suburb + '\'' +
+                ", type=" + type +
+                ", city='" + city + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                '}';
     }
 
-    public Long getAddressID() {
+    public long getAddressID() {
         return addressID;
     }
 
@@ -32,6 +51,10 @@ public class Address {
         return complexDetail;
     }
 
+    public Type getType() {
+        return type;
+    }
+
     public String getSuburb() {
         return suburb;
     }
@@ -40,65 +63,57 @@ public class Address {
         return city;
     }
 
-    public String getType() {
-        return type;
+    public String getPostalCode() {
+        return postalCode;
     }
-
-
-    @Override
-    public String toString() {
-        return "Address{" +
-                "addressID=" + addressID +
-                ", streetAddress='" + streetAddress + '\'' +
-                ", complexDetail='" + complexDetail + '\'' +
-                ", suburb='" + suburb + '\'' +
-                ", city='" + city + '\'' +
-                ", type='" + type + '\'' +
-                '}';
-    }
-
-
     public static class Builder {
-
         private long addressID;
         private String streetAddress;
         private String complexDetail;
         private String suburb;
-        private String city;
         private Type type;
+        private String city;
 
-        public Builder addressID(Long addressID) {
+        public Builder setAddressID(long addressID) {
             this.addressID = addressID;
             return this;
         }
 
-        public Builder streetAddress(String streetAddress) {
+        public Builder setStreetAddress(String streetAddress) {
             this.streetAddress = streetAddress;
             return this;
         }
 
-        public Builder complexDetail(String complexDetail) {
-            this.complexDetail = complwxDetail;
+        public Builder setComplexDetail(String complexDetail) {
+            this.complexDetail = complexDetail;
             return this;
         }
 
-        public Builder suburb(String suburb) {
+        public Builder setSuburb(String suburb) {
             this.suburb = suburb;
             return this;
         }
 
-        public Builder city(String city) {
-            this.city = city;
-            return this;
-        }
-
-        public Builder type(Type type) {
+        public Builder setType(Type type) {
             this.type = type;
             return this;
         }
 
+        public void setCity(String city) {
+            this.city = city;
+        }
+        public Builder copy(Address address) {
+        this.addressID = address.addressID;
+        this.streetAddress = address.streetAddress;
+        this.complexDetail = address.complexDetail;
+        this.suburb = address.suburb;
+        this.type = address.type;
+        this.city = address.city;
+        return this;
+        }
         public Address build() {
-            return new Address(this);
+       return new Address(this);
         }
     }
 }
+
