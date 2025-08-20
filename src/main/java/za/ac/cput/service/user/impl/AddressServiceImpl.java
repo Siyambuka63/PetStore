6592ee95e44f7fdf17ea8c895747470a844849a1
AthieX-34 <165181664+AthieX-34@users.noreply.github.com>
2025-08-20 21:39:32 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/service/user/impl/AddressServiceImpl.java
@@ -0,0 +1,58 @@
+package za.ac.cput.service.user.impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.user.Address;
+import za.ac.cput.repository.user.AddressRepository;
+import za.ac.cput.service.user.AddressService;
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    private static AddressService addressService;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    public AddressServiceImpl(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+
+   @Override
+   public Address create(Address address) {
+        return addressRepository.save(address);
+    }
+
+   @Override
+   public Address read(Long addressId) {
+        return addressRepository.getReferenceById(addressId);
+    }
+
+ @Override
+ public Address update(Address address) {
+        return addressRepository.save(address);
+    }
+@Override
+public void delete(Long addressId) {
+        addressRepository.deleteById(addressId);
+    }
+
+
+    @Override
+    public List<Address> getAll() {
+        return addressRepository.findAll();
+    }
+
+    @Override
+    public Address findByAddressId(Long addressId) {
+        return addressRepository.findByAddressId(addressId);
+    }
+
+    @Override
+    public Address findByPostalCode(String postalCode) {
+        return addressRepository.findByPostalCode(postalCode);
+    }
+}
