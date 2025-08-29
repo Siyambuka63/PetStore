37742eddd607beec7e2d74ad3191a916f4e3dcab
Siyambuka63 <mbalisiyambuka1@gmail.com>
2025-08-29 19:15:01 +0200

    Wishlist page progress

--- src/frontend/src/pages/WishlistPage.vue
+++ src/frontend/src/pages/WishlistPage.vue
@@ -1,58 +1,159 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <HeaderComponent/>
+
+  <div class="container">
+    <h1>My Wishlist</h1>
+    <div class="wishlist">
+      <div v-if="items.length === 0">You have no items wishlisted</div>
+      <div v-for="(item, index) in items" :key="index" class="wishlist-item">
+        <div class="left-section">
+          <img class="icon" src="@/assets/logo.png" v-bind:alt="item.name">
+        </div>
+
+        <div class="info">
+          <h3 v-text="item.name"></h3>
+          <p v-if="item.on_sale">
+            Was: <s>R{{ item.price.toFixed(2) }}</s>
+            Now: R{{ item.sale_price.toFixed(2) }}
+          </p>
+          <p v-else v-text="'R' + item.price.toFixed(2)"></p>
+        </div>
+
+        <div class="rating">
+          <img v-for="i in Math.ceil(item.rating)" :key="'filled-' + i" class = "stars" src="@/assets/star_filled.png" alt="Filled Star">
+
+          <img v-for="i in Math.ceil(5- item.rating)" :key="'empty-' + i" class = "stars" src="@/assets/star_empty.png" alt="Empty Star">
+        </div>
+
+        <div class = "buttons">
+          <button v-if = "item.stock >= 1" id="add_button">Add to Cart</button>
+          <p v-else>SOLD OUT</p>
+          <button id="remove_button">Remove</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "WishlistPage",
+  components: {HeaderComponent},
+  data() {
+    return {
+      items: [
+        {
+          image: "",
+          name: "Wireless Headphones",
+          on_sale: false,
+          price: 200.0,
+          sale_price: 180.0,
+          rating: 3.5,
+          stock: 3
+        },
+        {
+          image: "",
+          name: "Wired Headphones",
+          on_sale: true,
+          price: 200.0,
+          sale_price: 180.0,
+          rating: 2.5,
+          stock: 3
+        },
+        {
+          image: "",
+          name: "Headphones",
+          on_sale: true,
+          price: 200.0,
+          sale_price: 150.0,
+          rating: 4.5,
+          stock: 3
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.wishlist {
+  width: 80%;
+  display: flex;
+  flex-direction: column;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.wishlist-item {
+  display: flex;
+  gap: 10%;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  margin: 5px;
+  border-radius: 8px;
+  border: 2px solid #dfe6e9;
 }
-a {
-  color: #42b983;
+
+.left-section {
+  align-items: center;
 }
+
+.icon {
+  height: 70px;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.rating {
+  display: flex;
+  flex-direction: row;
+}
+
+.stars {
+  height: 30px;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+button {
+  border: none;
+  border-radius: 8px;
+  margin: 5px;
+  padding: 10px;
+  color: white;
+  width: 100px;
+}
+
+#add_button {
+  background: #0984e3;
+}
+
+#add_button:active, #add_button:hover {
+  background: #0652DD;
+}
+
+#remove_button {
+  background: #FF2D2D;
+}
+
+#remove_button:active, #remove_button:hover {
+  background: red;
+}
+
 </style>
