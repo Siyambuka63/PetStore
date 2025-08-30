e2ea52010445b03c0897c8d562f9b094620db4ff
nkheso <230762883@mycput.ac.za>
2025-08-30 11:22:06 +0200

    updated login page

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -2,19 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import * as VueRouter from 'vue-router'
 
+import LogInPage from "./pages/LogInPage.vue";
 import ProfilePage from "./pages/ProfilePage.vue";
 import WishlistPage from "./pages/WishlistPage.vue";
-
+import SignUpPage from "./pages/SignUpPage.vue";
 createApp(App)
     .use(VueRouter.createRouter({
         history: VueRouter.createWebHistory(process.env.BASE_URL),
-        routes: [{
+        routes: [
+            { path: "/", name: "LogIn",
+                component: LogInPage },
+            {
             path: '/profile',
             component: ProfilePage
         },
             {
                 path: '/wishlist',
                 component: WishlistPage
-            }]
+            },
+            { path: "/signup", name: "SignUp",
+                component: SignUpPage }]
     }))
     .mount('#app')
