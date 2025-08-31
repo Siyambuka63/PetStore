04c6b0c7d8d93f397d4f6b92f6ba01c0bd834e1e
Siyambuka63 <mbalisiyambuka1@gmail.com>
2025-08-31 22:08:28 +0200

    Updated Wishlist

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -8,6 +8,7 @@ import ProfilePage from "./pages/ProfilePage.vue";
 import WishlistPage from "./pages/WishlistPage.vue";
 import SignUpPage from "./pages/SignUpPage.vue";
 import LogInPage from "./pages/LogInPage.vue";
+import ProductsPage from "@/pages/ProductsPage.vue";
 
 const router = VueRouter.createRouter({
     history: VueRouter.createWebHistory(process.env.BASE_URL),
@@ -29,10 +30,7 @@ const router = VueRouter.createRouter({
         },
         {
             path: '/ProductsPage',
-            component: ProductsPage},
-        {
-            path: '/wishlist',
-            component: WishlistPage
+            component: ProductsPage
         },
         { 
             path: "/signup",
@@ -45,7 +43,7 @@ const router = VueRouter.createRouter({
 router.beforeEach((to, from, next) => {
     const user = useAuth()
 
-    if (to.meta.requiresAuth && !user.userId) {
+    if (to.meta.requiresAuth && !user.userID) {
         // redirect to log in page
         next({ name: "LogIn" })
     } else {
