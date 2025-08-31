7c429f69a6d2f793e31974fabcc4e6573faac6df
Sinovuyo123 <sinovuyomathungana@gmail.com>
2025-08-31 23:41:02 +0200

    seperated orderItems from orders and added orderstore to track the data accross states

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -4,17 +4,24 @@ import * as VueRouter from 'vue-router'
 import { createPinia } from 'pinia'
 import { useAuth } from "@/Auth";
 
+
 import ProfilePage from "./pages/ProfilePage.vue";
 import WishlistPage from "./pages/WishlistPage.vue";
 import SignUpPage from "./pages/SignUpPage.vue";
-import LogInPage from "./pages/LogInPage.vue";
-import ProductsPage from "./pages/ProductsPage.vue";
-
+import OrdersPage from "./pages/OrdersPage.vue";
+import OrderItems from "./pages/OrderItems.vue";
+import LogInPage from "@/pages/LogInPage.vue";
 const router = VueRouter.createRouter({
     history: VueRouter.createWebHistory(process.env.BASE_URL),
     routes: [
+
         {
-            path: "/",
+            path: "/orderPage",
+            name: "orderPage",
+            component: OrdersPage
+        },
+        {
+            path: "/login",
             name: "LogIn",
             component: LogInPage
         },
@@ -29,19 +36,16 @@ const router = VueRouter.createRouter({
             meta: { requiresAuth: true }
         },
         {
-            path: '/ProductsPage',
-            component: ProductsPage},
-        {
-            path: '/wishlist',
-            component: WishlistPage
+            path: '/orderItem',
+            component: OrderItems
         },
-        { 
+        {
             path: "/signup",
             name: "SignUp",
-            component: SignUpPage 
+            component: SignUpPage
         }
     ]
- })
+})
 
 router.beforeEach((to, from, next) => {
     const user = useAuth()
